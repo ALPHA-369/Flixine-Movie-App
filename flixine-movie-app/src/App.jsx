2486f6982ba311c0ca3bb478054070d2ff1d05ff
ALPHA369 <ucjunior369@gmail.com>
2026-02-08 16:55:25 +0100

    feat: add search component and integrate FontAwesome icons

--- flixine-movie-app/src/App.jsx
+++ flixine-movie-app/src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
+import Search from "./components/search";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {}, []);
   return (
     <main>
       <div className="pattern" />
@@ -8,11 +13,12 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero banner" />
           <h1>
-            Explore our Galore of <span className="text-gradient">movies</span>{" "}
-            and <span className="text-gradient">TV shows</span> you'll love!
+            Explore our vast collection of awesome{" "}
+            <span className="text-gradient">movies</span> & {""}
+            <span className="text-gradient">TV shows</span>!
           </h1>
         </header>
-        <p>search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
     </main>
   );
---
+++ flixine-movie-app/src/components/Search.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fas, far, fab);
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <FontAwesomeIcon
+          icon="fa-solid fa-magnifying-glass"
+          className="search-icon"
+        />
+        <input
+          type="text"
+          placeholder="Search our vast collection of movies and TV shows!"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Search;
